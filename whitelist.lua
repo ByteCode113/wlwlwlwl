4b15762df3ef85badd33d058bc90dce55008cc74
ByteCode <113543570+ByteCode113@users.noreply.github.com>
2026-07-30 19:54:53 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,3 +1,4 @@
 return {
-    "0be9fe32f90d450d0bd4a996160bc0d93144150f24b27d836ea091a21fbb35cf" -- meow niga
+    "0be9fe32f90d450d0bd4a996160bc0d93144150f24b27d836ea091a21fbb35cf", -- meow niga
+    "c59d4f1f5cb88cf80f1902c783f5ebaa52afe7ad524cb17adcd535271c73a2f9" -- l
 }
