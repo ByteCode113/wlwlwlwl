fdfa15f8a5f6863b837b06a0b510baa8442dc328
ByteCode <113543570+ByteCode113@users.noreply.github.com>
2026-08-03 21:48:44 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -2,6 +2,7 @@ return {
     "c963d4b025b96270cdc8b6bba1d4e10e2aa849c761756842cea0a471dcab195c", -- meow niga
     "464ae9ab8964b0da70fa414265c1d2e83a5cd2416e954da6bec562a927201eaa", -- mister robot bro trust
     "c59d4f1f5cb88cf80f1902c783f5ebaa52afe7ad524cb17adcd535271c73a2f9", -- byte
+    "1cf8c8cebab7aa7430827863076869a7e92f26c14c767b1cf72e3ad7907ae59b", -- vope
     "c12304ad3f463c661634d30112d065a6ec1c6fa0ad36f1fc3d965f284cdc4784" -- gktgzk yes
 }
 -- hi
